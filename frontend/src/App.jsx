737e8b7a1b069920f92fd9f81d39edd9f8d1582b
Prasanna Vathana <prasannavathana33@gmail.com>
2025-06-28 13:59:32 +0530

    frontend-setup

---
+++ frontend/src/App.jsx
@@ -0,0 +1,15 @@
+
+import './App.css'
+
+function App() {
+
+
+  return (
+    <button className="bg-blue-500 text-white px-4 py-2 rounded">
+      Hello Tailwind
+    </button>
+  )
+  
+}
+
+export default App
